794988fc603b988a686759fc19ed1c02614335c0
Jan Andersson <jan.andersson@gmail.com>
2016-03-22 11:49:11 +0100

    Move fixtures into own file
    And minor cleanup

--- index.ios.js
+++ index.ios.js
@@ -2,8 +2,6 @@
 import React, {
   AppRegistry,
   Component,
-  StyleSheet,
-  Text,
   View
 } from 'react-native';
 import ChildSelector from './ChildSelector';
@@ -11,56 +9,22 @@ import Toolbar from './Toolbar';
 import TransactionList from './TransactionList';
 
 var PiggyBank = React.createClass({
+  data: require('./fixtures/data.json'),
   getInitialState() {
-    return {
-      active: "Vilgot"
-    }
-  },
-  data: {
-    children: [
-      {
-        name: "Vilgot",
-        avatar_uri: "https://www.dropbox.com/s/r11h9vz2t9i7mwk/vilgot.png?dl=1",
-      },
-      {
-        name: "Elze",
-        avatar_uri: "https://www.dropbox.com/s/xbb8vnrof51zzxj/elze.png?dl=1",
-      },
-    ],
-    transactions: [
-      { description: 'From piggybank', amount: 373, currency: 'kr', children: ['Vilgot'] },
-      { description: 'From piggybank', amount: 5, currency: 'kr', children: ['Elze'] },
-      { description: 'From aunt Emilia', amount: 530, currency: 'kr', children: ['Vilgot', 'Elze'] },
-      { description: 'Nerf', amount: -170, currency: 'kr', children: ['Vilgot'] },
-      { description: 'Lego to Theo', amount: 200, currency: 'kr', children: ['Vilgot'] },
-      { description: 'Tooth Fairy', amount: 20, currency: 'kr', children: ['Vilgot'] },
-      { description: 'Turtle', amount: -35, currency: 'kr', children: ['Elze'] },
-      { description: 'Kitty keyring', amount: -49, currency: 'kr', children: ['Elze'] },
-      { description: 'Crown, drawing pad, pearls', amount: -138, currency: 'kr', children: ['Elze'] },
-      { description: 'Easter Gift', amount: 50, currency: 'kr', children: ['Vilgot', 'Elze'] },
-      { description: 'Birthday', amount: 500, currency: 'kr', children: ['Vilgot'] },
-      { description: 'Gift from Severina', amount: 472, currency: 'kr', children: ['Vilgot', 'Elze'] },
-      { description: 'Gift from Povilas', amount: 377, currency: 'kr', children: ['Vilgot', 'Elze'] },
-      { description: 'Candy', amount: -28, currency: 'kr', children: ['Vilgot'] },
-    ]
+    return { active: "Vilgot" }
   },
   changePage(page) {
     this.setState({active: this.data.children[page].name});
   },
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Toolbar />
         <ChildSelector onPageChange={this.changePage} {...this.data} />
         <TransactionList active={this.state.active} {...this.data} />
       </View>
     );
   }
-});
-
-const styles = StyleSheet.create({
-  container: {
-  },
 });
 
 AppRegistry.registerComponent('PiggyBank', () => PiggyBank);
